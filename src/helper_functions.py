b735913ecf39a3bc8f0e8c00ba4445fe4c07fa17
matthiasf1 <matthias.fauler@gmail.com>
2024-02-23 16:53:35 +0100

    Added SeqSelfAttention layer

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -13,3 +13,30 @@ def plot_history(history):
         plt.show()
     except:
         print('No history to plot')
+
+
+def save_model(model, model_name, src_dir):
+    # Save the model to a specified directory
+    if CODE_ENV=='local':
+        model.save(src_dir + 'models/' + model_name + '.h5')
+        
+    if CODE_ENV=='kaggle':
+        model.save('/kaggle/working/' + model_name + '.h5')
+
+    if CODE_ENV=='aws':
+        model.save(src_dir + 'models/' + model_name + '.h5')
+
+
+def load_model(model_name, src_dir):
+    # Start from here if you want to load the model
+    # Load the model from a specified directory
+    if CODE_ENV=='local':
+        model = load_model(src_dir + 'models/' + model_name + '.h5', custom_objects={'rmse': rmse})
+
+    if CODE_ENV=='kaggle':
+        model = load_model('/kaggle/input/v1-model/' + model_name + '.h5', custom_objects={'rmse': rmse})
+
+    if CODE_ENV=='aws':
+        model = load_model(src_dir + 'models/' + model_name + '.h5', custom_objects={'rmse': rmse})
+
+    return model
